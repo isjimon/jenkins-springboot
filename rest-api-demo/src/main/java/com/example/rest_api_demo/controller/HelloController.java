b4bb783633dca08bc3b59a8b1834c49e9efb616b
AI Sigfred Jr Balatan <aisigfredjrbalatan@192-169-5-171-static.despegar.net>
2026-04-08 09:42:06 +0800

    added API endpoint

--- rest-api-demo/src/main/java/com/example/rest_api_demo/controller/HelloController.java
+++ rest-api-demo/src/main/java/com/example/rest_api_demo/controller/HelloController.java
@@ -2,14 +2,26 @@ package com.example.rest_api_demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.rest_api_demo.model.Greeting;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
 
+    private final AtomicLong counter = new AtomicLong();
+
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World!";
+    }
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format("Hello, %s!", name));
     }
 }
---
+++ rest-api-demo/src/main/java/com/example/rest_api_demo/model/Greeting.java
@@ -0,0 +1,19 @@
+package com.example.rest_api_demo.model;
+
+public class Greeting {
+        private long id;
+        private String message;
+
+        public Greeting(long id, String message) {
+            this.id = id;
+            this.message = message;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+}
